590b80da38a95e39b8566c6bea33fdc3e5141976
bokunopeko <159253544+bokunopeko@users.noreply.github.com>
2024-03-07 11:47:38 +1100

    it works!

--- Assets/CameraSwapper.cs
+++ Assets/CameraSwapper.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
 
 public class CameraSwapper : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Camera firstPersonCamera;
+    [SerializeField] private Camera thirdPersonCamera;
+    [SerializeField] private FirstPersonController firstPersonController;
+    [SerializeField] private ThirdPersonController thirdPersonController;
+
+
+    public enum CameraMode
     {
-        
+        FirstPerson,
+        ThirdPerson
     }
 
-    // Update is called once per frame
+    [SerializeField] private CameraMode currentCameraMode;
+
+    void Start()
+    {
+        firstPersonController = GetComponent<FirstPersonController>();
+        thirdPersonController = GetComponent<ThirdPersonController>();
+
+        //find the camera scripts first, and get the component off the same object
+        firstPersonCamera = FindObjectOfType<FirstPersonCamera>().GetComponent<Camera>();
+        thirdPersonCamera = FindObjectOfType<ThirdPersonCamera>().GetComponentInChildren<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            ToggleCamera();
+        }
+
+    }
+
+    private void ToggleCamera()
+    {
+        if (currentCameraMode == CameraMode.FirstPerson)
+        {
+            currentCameraMode = CameraMode.ThirdPerson;
+        }
+        else
+        {
+            currentCameraMode = CameraMode.FirstPerson
+        }
+
+        SetCamera();
+    }
+    
+    private void SetCamera()
+    {
+
     }
 }
